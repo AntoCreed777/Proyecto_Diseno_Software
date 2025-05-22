1c8219a485344db5f07f22ef9e246f9378562520
Luis <lumartinez2023@udec.cl>
2025-05-22 19:46:05 -0400

    feat: Se implemento la funcionalidad de registrarse

--- Proyecto_Diseño_Software/urls.py
+++ Proyecto_Diseño_Software/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('maps/', include('maps.urls')),  # Incluye las URLs de la app de mapas, se concatenan con el prefijo 'maps/'
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/',views.Temporal),
+    path('accounts/register/',views.registration), 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
 # Create your views here.
+def Temporal(request):
+     return render(request, 'login.html')
+def registration(request):
+     if request.method == 'POST':
+          try:
+               user = User.objects.create_user(username=request.POST['usuario'],password=request.POST['contraseña'])
+               return HttpResponse('Usuario creado')
+          except:
+               return HttpResponse('Fallo')
+     return render(request, 'registration.html')
