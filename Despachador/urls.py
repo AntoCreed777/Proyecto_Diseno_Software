4247f592a58441aeb541c847b4e5a1c4b68744dc
Arcisternas <arcisternas2023@udec.cl>
2025-06-05 15:33:57 -0400

    feat: Registrar paquetes y clientes
    -Se le añade la funcionalidad al botón de registrar paquete, dirigiendo a un formulario con toda la información a rellenar.
    -Se implementa un botón para poder registrar clientes en caso de no estarlo para poder asignarles el pedido a registrar

--- Despachador/urls.py
+++ Despachador/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import inicio, paquetes, conductores
+from .views import inicio, paquetes, conductores, registrar_paquete, registrar_cliente
 
 urlpatterns = [
     path('inicio/', inicio, name = 'inicio_despachador'),
     path('paquetes/', paquetes, name = 'paquetes_despachador'),
     path('conductores/', conductores, name = 'conductores_despachador'),
+    path('registrar-paquete/', registrar_paquete, name='registrar_paquete_despachador'),
+    path('registrar-cliente/', registrar_cliente, name='registrar_cliente_despachador'),
 ]
--- Despachador/views.py
+++ Despachador/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from api.models import Paquete, Cliente, Usuario
 
 # Create your views here.
 
@@ -10,9 +11,47 @@ def inicio(request):
     return render(request,'despachador/inicio.html')
 
 def paquetes(request):
-
-    return render(request,'despachador/paquetes.html')
+    from api.models import Cliente, Paquete
+    clientes = Cliente.objects.select_related('usuario').all()
+    return render(request,'despachador/paquetes.html', {
+        'clientes': clientes,
+        })
 
 def conductores(request):
 
     return render(request,'despachador/conductores.html')
+
+def registrar_paquete(request):
+    if request.method == 'POST':
+        Paquete.objects.create(
+            dimensiones= request.POST.get('dimensiones'),
+            peso=request.POST.get('peso'),
+            ubicacion_actual_lat=0.0,
+            ubicacion_actual_lng=0.0,
+            direccion_envio_lat=0.0,
+            direccion_envio_lng=0.0,
+            nombre_destinatario= request.POST.get('nombre_destinatario'),
+            rut_destinatario= request.POST.get('rut_destinatario'),
+            telefono_destinatario= request.POST.get('telefono_destinatario'),
+            cliente_id=request.POST.get('cliente_id'),
+            despachador_id=Despachador.objects.get(usuario=request.user).id,
+        )
+        return redirect('paquetes_despachador')
+    return redirect('paquetes_despachador')
+def registrar_cliente(request):
+    if request.method == 'POST':
+        usuario = Usuario.objects.create(
+            username=request.POST.get('username'),
+            first_name=request.POST.get('nombre'),
+            last_name=request.POST.get('apellido'),
+            email=request.POST.get('correo'),
+            password=request.POST.get('contraseña'),
+            telefono=request.POST.get('telefono'),
+            rol='cliente'
+        )
+        Cliente.objects.create(
+            usuario=usuario,
+            direccion_hogar= request.POST.get('direccion')
+        )
+        return redirect('paquetes_despachador')
+    return redirect('paquetes_despachador')
