61b0ee6ea280eaa02e050a9cb1ec2d3dc15c7639
joacosando <joaquinsandovalreyes04@gmail.com>
2025-06-10 20:09:36 -0400

    feat: Visualizacion de la tabla con la base de datos

--- Cliente/models.py
+++ Cliente/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class paquete(models.Model):
-    id = models.IntegerField(primary_key = True)
-    ubicacion_actual = models.CharField(max_length = 255)
-    fecha_envio = models.DateField()
-    direccion_envio = models.CharField(max_length = 255)
-    dimensiones = models.FloatField()
-    peso = models.FloatField()
--- Cliente/views.py
+++ Cliente/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from api.models import Cliente
+from api.models import Cliente, Paquete
 
 # Create your views here.
 def inicio(request):
@@ -7,13 +7,12 @@ def inicio(request):
     return render(request,'Cliente/inicio.html')
 
 def mis_pedidos(request):
-    if hasattr(request.user, 'cliente'):
-        clientes = Cliente.objects.all()
-    else:
-        clientes = "No hay clientes"
+
+    clientes = Cliente.objects.all()
+    paquetes = Paquete.objects.all()
 
     print(clientes)
-    return render(request, 'Cliente/mis_pedidos.html')
+    return render(request, 'Cliente/mis_pedidos.html', context = {'paquetes':paquetes}, )
 
 def ayuda(request):
 
