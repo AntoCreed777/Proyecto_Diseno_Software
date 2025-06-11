3a05b6014a1529e040ac129d024558bf345e634f
AntoCreed777 <abenavides@desem.cl>
2025-06-11 18:25:59 -0400

    build: Mejora en el script init_venv

--- init_venv.sh
+++ init_venv.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 echo "Sistema operativo detectado: $(uname -s)"
-if [[ "$(uname -s)" == "Linux" || "$(uname -s)" == "Darwin" ]]; then
+OS_NAME="$(uname -s)"
+
+if [[ "$OS_NAME" == "Linux" || "$OS_NAME" == "Darwin" ]]; then
     echo "Activando entorno virtual en Linux/macOS..."
     source venv/bin/activate || { echo "Fallo al activar el entorno virtual."; exit 1; }
-elif [[ "$(uname -s)" == *"NT"* || "$(uname -s)" == *"MINGW"* || "$(uname -s)" == *"CYGWIN"* ]]; then
+elif [[ "$OS_NAME" == *"NT"* || "$OS_NAME" == *"MINGW"* || "$OS_NAME" == *"CYGWIN"* ]]; then
     echo "Activando entorno virtual en Windows..."
-    . venv/Scripts/activate || { echo "Fallo al activar el entorno virtual."; exit 1; }
+    source venv/Scripts/activate || { echo "Fallo al activar el entorno virtual."; exit 1; }
 else
     echo "Sistema operativo no compatible."
     exit 1
 fi
 
-pip install -r requirements.txt || { echo "Fallo al instalar dependencias."; exit 1; }
+echo "Instalando dependencias necesarias..."
+if ! pip install -r requirements.txt; then
+    echo "Fallo al instalar dependencias."
+    exit 1
+fi
 
 echo "Entorno virtual activado y dependencias instaladas."
